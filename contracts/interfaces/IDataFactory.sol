ca578e5dad900dc5e24a729eab33250e16d34438
juwitaw <juwita.winadwiastuti@dattabot.io>
2018-12-27 19:30:01 +0700

    refactor

--- contracts/interfaces/IDataFactory.sol
+++ contracts/interfaces/IDataFactory.sol
@@ -14,7 +14,8 @@ interface IDataFactory {
         address location, bytes signature, bytes signatureFunc
     );    
    
-    function storeData(address _owner, address _location, bytes _signature) external returns (address dataStoreContract);
-    function storeData(address _owner, address _location, bytes _signature, bytes _signatureFunc) external returns (address dataStoreContract);
-    // function getDataTotalByOwner(address _owner) external view returns (uint256);
+    function storeData(address _owner, address _location, bytes _signature) external 
+    returns (address dataStoreContract);
+    function storeData(address _owner, address _location, bytes _signature, bytes _signatureFunc) external 
+    returns (address dataStoreContract);
 }
